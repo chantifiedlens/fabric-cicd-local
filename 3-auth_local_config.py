47b43e91f44046de51230103676efe6e4d23a926
ChantifiedLens <kchantnl@outlook.com>
2026-02-19 14:42:40 +0100

    Update script execution syntax and clean up feature flag comments

--- 3-auth_local_config.py
+++ 3-auth_local_config.py
@@ -1,13 +1,13 @@
 # Log in with Azure CLI (az login) prior to execution
-# Syntax to run 'python .\auth_local_hard_coded.py'
+# Syntax to run 'python .\3-auth_local_config.py'
 
 from pathlib import Path
 
-# from azure.identity import AzureCliCredential
 from fabric_cicd import deploy_with_config,append_feature_flag
 
 append_feature_flag("enable_experimental_features")
-append_feature_flag("enable_config_deploy")
+# Commented out as no longer required from fabric-cicd v0.2.0 onwards for configuration based deployments
+# append_feature_flag("enable_config_deploy")
 
 # Deploy using a config file
 deploy_with_config(
